054479c686688c961d234dfc2e494c685e396e19
Shobhit000s <shobhittripathi05@gmail.com>
2026-04-10 14:53:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ const http = require('http');
 http.createServer((req, res) => {
   res.write("Hello from Docker + Jenkins!");
   res.wrire("this is the updated one for the testing");
+  res.wrire("this is the updated one for the testing");
   res.end();
 }).listen(3000);
